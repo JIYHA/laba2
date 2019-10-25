74af24d5d5010895c028328f7bd706382657decf
JIYHA <56276037+JIYHA@users.noreply.github.com>
2019-10-25 12:28:28 +0300

    Add files via upload

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+            double c = Convert.ToInt32(Console.ReadLine());
+            double d = (b * b - 4 * a * c);
+            if (d > 0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("x1=" + x1 + "x2=" + x2);
+
+            }
+            else if (d == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("x=" + x);
+            }
+            else
+            {
+                double y = 0;
+
+                Console.WriteLine("Answer is x + i * y");
+                Console.WriteLine("Answer is x - i * y");
+            }
+        }
+    }
+}
